23600c4df64d739d005d3579f247536a0187d2ee
Tiago Rocha Gomes <tiagorg@ciandt.com>
2021-11-19 14:36:00 -0300

    Toggle of schedule execution

--- metrics-service/src/app.ts
+++ metrics-service/src/app.ts
@@ -13,6 +13,7 @@ async function main() {
   }
 }
 
-main()
+main();
+/*main()
   .then(() => schedule(cron, main))
-  .catch(err => logger.error(err, `Error in scheduling execution.`));
+  .catch(err => logger.error(err, `Error in scheduling execution.`));*/
